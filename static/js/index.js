7173c534c8b6eda8f75a034b702f51f7f2e617e9
ishcha <isha4@illinois.edu>
2026-03-10 13:29:29 -0400

    remade webpage

--- static/js/index.js
+++ static/js/index.js
@@ -18,4 +18,25 @@ $(document).ready(function() {
 	
     bulmaSlider.attach();
 
+    // Simple overview slideshow from exported PPT slides
+    var slideshowImg = document.getElementById('overview-slideshow');
+    // Slides are expected at:
+    // static/images/lumos-overview-slides/lumos_slides/Slide<number>.png
+    var lumosSlideCount = 1; // TODO: set this to the number of slides you add
+    var slidePaths = [];
+    for (var i = 1; i <= lumosSlideCount; i++) {
+      slidePaths.push('static/images/lumos-overview-slides/lumos_slides/Slide' + i + '.png');
+    }
+
+    if (slideshowImg && slidePaths.length > 0) {
+      var idx = 0;
+      slideshowImg.src = slidePaths[0];
+      if (slidePaths.length > 1) {
+        setInterval(function() {
+          idx = (idx + 1) % slidePaths.length;
+          slideshowImg.src = slidePaths[idx];
+        }, 5000); // 5 seconds per slide
+      }
+    }
+
 })
